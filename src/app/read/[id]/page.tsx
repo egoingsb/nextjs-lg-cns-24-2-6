df57a7172d1d0bdb377e6e690af87c0788d2bf7e
egoinig <egoing@gmail.com>
2024-02-06 17:18:13 +0900

    글 수정 기능 구현

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -1,8 +1,7 @@
 // @ts-ignore
 export default async function Read(props){
-  const resp = await fetch('http://localhost:9999/page/'+props.params.id);
+  const resp = await fetch('http://localhost:9999/page/'+props.params.id, {next:{revalidate:0}});
   const result = await resp.json();
-  console.log('result', result);
   return <div>
     <h2>{result.title}</h2>
     {result.body}
--- src/app/update/[id]/page.tsx
+++ src/app/update/[id]/page.tsx
@@ -1,15 +1,58 @@
+'use client'
+
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+
 type UpdateProps = {
   params: {
     id: string
   }
 }
 export default function Update(props:UpdateProps){
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const router = useRouter();
+  useEffect(()=>{
+    fetch('http://localhost:9999/page/'+props.params.id)
+      .then(resp=>resp.json())
+      .then(data=>{
+        setTitle(data.title);
+        setBody(data.body);
+      })
+  },[])
+  // @ts-ignore
+  async function submitHandler(evt){
+    evt.preventDefault();
+    const resp = await fetch('http://localhost:9999/page/'+props.params.id,{
+      method:'PUT',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({title, body})
+    })
+    const result = await resp.json();
+    router.push('/read/'+result.id);
+    router.refresh();
+  }
   return <div>
-    <h2>Update {props.params.id}</h2>
+    <h2>Update</h2>
+    <form onSubmit={submitHandler}>
+      <p>
+        <input 
+          type="text" 
+          name="title" 
+          placeholder="title" 
+          value={title} 
+          onChange={evt=>setTitle(evt.target.value)} 
+        /></p>
+      <p>
+        <textarea
+          name="body"
+          placeholder="body"
+          onChange={evt=>setBody(evt.target.value)}
+          value={body}></textarea>
+      </p>
+     <p><input type="submit" value="update" /></p>
+    </form>
   </div>
 }
-
-// function sum(left:number, right:number):number{
-//   return left + right;
-// }
-// sum(1, 'two');
