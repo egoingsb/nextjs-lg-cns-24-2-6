2e129bfced093270e181570f2f550a87dee5c327
egoinig <egoing@gmail.com>
2024-02-06 11:43:34 +0900

    a태그를 Link 컴포넌트로 바꾸면,
    Single Page Application (SPA)를 구현할 수 있다.
    
    사용자의 입력을 유지시키줌.
    바뀌는 페이지만 부분적으로 다운로드 함
    이미 방문한 페이지를 캐쉬를 이용함.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
 
 
 export const metadata: Metadata = {
@@ -16,21 +17,22 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <header>
-          <h1><a href="/">WEB!</a></h1>
+          <h1><Link href="/">WEB!</Link></h1>
+          <input type="text" placeholder="search?" />
         </header>
         <nav>
           <ol>
-            <li><a href="/read/1">html</a></li>
-            <li><a href="/read/2">css</a></li>
-            <li><a href="/read/3">js</a></li>
+            <li><Link href="/read/1">html</Link></li>
+            <li><Link href="/read/2">css</Link></li>
+            <li><Link href="/read/3">js</Link></li>
           </ol>
         </nav>
         <article>
           {children}
         </article>
         <ul>
-          <li><a href="/create">create</a></li>
-          <li><a href="/update/id">update</a></li>
+          <li><Link href="/create">create</Link></li>
+          <li><Link href="/update/id">update</Link></li>
           <li><button>delete</button></li>
         </ul>
       </body>
