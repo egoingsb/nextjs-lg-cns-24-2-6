4dd0872b97b538ff5cfd670aa0480e2c4815bdfa
egoinig <egoing@gmail.com>
2024-02-06 15:02:27 +0900

    글 보기 구현

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -1,6 +1,10 @@
 // @ts-ignore
-export default function Read(props){
+export default async function Read(props){
+  const resp = await fetch('http://localhost:9999/page/'+props.params.id);
+  const result = await resp.json();
+  console.log('result', result);
   return <div>
-    <h2>Read {props.params.id}</h2>
+    <h2>{result.title}</h2>
+    {result.body}
   </div>
 }
